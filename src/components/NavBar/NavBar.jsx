a923aedb4854398330d71a3edee8df4c85d300f8
David Colatti <davidcolatti@gmail.com>
2020-08-03 16:37:26 -0400

    feat: set up of mdbreact and responsive navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.scss";
 import emblem from "./../../assets/images/app-emblem.png";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBIcon,
+} from "mdbreact";
 
 const NavBar = () => {
+  const [isOpen, setToggle] = useState(false);
+
+  const toggleCollapse = () => {
+    setToggle(!isOpen);
+  };
+
   return (
-    <div className={styles.navBar}>
-      <img className={styles.emblem} src={emblem} alt="app-emblem" />
-    </div>
+    <MDBNavbar className={styles.navBar} dark expand="md">
+      <MDBNavbarBrand>
+        <img src={emblem} className={styles.emblem} alt="emblem" />
+      </MDBNavbarBrand>
+      <MDBNavbarToggler onClick={toggleCollapse} />
+      <MDBCollapse
+        id="navbarCollapse3"
+        isOpen={isOpen}
+        navbar
+        className={isOpen && styles.navBarCollapse}
+      >
+        <MDBNavbarNav left>
+          <MDBNavItem>
+            <MDBNavLink to="#!">Home</MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#!">Features</MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#!">Pricing</MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBDropdown>
+              <MDBDropdownToggle nav caret>
+                <div className="d-none d-md-inline">Dropdown</div>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className="dropdown-default">
+                <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </MDBNavItem>
+        </MDBNavbarNav>
+        <MDBNavbarNav right>
+          <MDBNavItem>
+            <MDBDropdown>
+              <MDBDropdownToggle nav caret>
+                <MDBIcon icon="user" className={styles.userIcon} />
+              </MDBDropdownToggle>
+
+              <MDBDropdownMenu className={styles.userDropDown}>
+                <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </MDBNavItem>
+        </MDBNavbarNav>
+      </MDBCollapse>
+    </MDBNavbar>
   );
 };
 
