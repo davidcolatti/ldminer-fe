26733c81be112740247fefd616a741784523c50c
David Colatti <davidcolatti@gmail.com>
2020-08-15 17:27:29 -0400

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Switch, Route } from "react-router-dom";
 import { UserContext } from "./providers/UserProvider";
 import actions from "./services";
 
-import NavBar from "./components/NavBar";
-import QuickPick from "./components/QuickPick";
-import Dashboard from "./components/Dashboard";
-import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar/NavBar";
+import QuickPick from "./components/QuickPick/QuickPick";
+import Dashboard from "./components/Dashboard/Dashboard";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 const App = () => {
   const userAuthenticaton = useContext(UserContext);
