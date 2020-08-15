b8407a4a4cf613961c797c7ac46f2b78aba1fd0a
David Colatti <davidcolatti@gmail.com>
2020-08-15 17:10:46 -0400

    fix: modified config for firebase to deploy

---
+++ src/config/Fire.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDf-36xHzaEE1Yz-RwIDesh-US5vXMynDY",
+  authDomain: "leadminerv2.firebaseapp.com",
+  databaseURL: "https://leadminerv2.firebaseio.com",
+  projectId: "leadminerv2",
+  storageBucket: "leadminerv2.appspot.com",
+  messagingSenderId: "923108816655",
+  appId: "1:923108816655:web:07cb12ef4ff9c17f6b707a",
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export const auth = firebase.auth();
+export const signInWithGoogle = () => {
+  auth.signInWithRedirect(provider);
+};
+
+export default fire;
